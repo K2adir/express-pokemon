519b642e8652d7c9177c545e3b754f596d95aa35
K2adir <k2adir@gmail.com>
2023-04-16 21:59:21 -0400

    data base connected

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require("express");
 const app = express();
 const PORT = 5009;
 //
+//models
+const pokemon = require("./models/pokemon");
 // ============== ENGINE ==================/
 app.set("view engine", "jsx");
 app.engine("jsx", require("jsx-view-engine").createEngine());
@@ -18,6 +20,10 @@ app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello</h1>");
+});
+// ============== / POKEMON
+app.get("/pokemon", (req, res) => {
+  res.send(pokemon);
 });
 
 app.listen(PORT, () => {
